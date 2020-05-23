d81f95b24385285779e16790dc4d13393dc97171
XxBoomerxX <tomyrm30@gmail.com>
2020-05-24 00:43:54 +0300

    handled decoding frame error

--- screens/video_chat.py
+++ screens/video_chat.py
@@ -88,7 +88,10 @@ class ChatWindow:
         pygame.quit()
 
     def __form_input(self, frame):
-        frame = cv2.imdecode(np.fromstring(frame, dtype=np.uint8), -1)
+        try:
+            frame = cv2.imdecode(np.fromstring(frame, dtype=np.uint8), -1)
+        except:
+            print('failed decoding frame')
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = np.rot90(frame)
         return frame
